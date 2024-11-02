2336c036128c9ff92db6c6bcbf5c243333f5a7f5
김정윤 <97427304+kjjyyy@users.noreply.github.com>
2024-11-02 14:51:55 +0900

    Update post_2screen.dart
    - 날짜란 색상 변경
    - 날짜란 크기 변경 (길게 늘여져있던거 줄이기)
    - 날짜 20NN년 10월 29일로 수정 (모든 게시물이 2024년에 올라온게 아니기 때문)
    - 좋아요 수 조정
    - 게시물 글씨 검은색으로 변경
    - 좋아요 관련 함수 변경 (post1이랑 동일한 함수, 변수 사용)

--- flutter/post_2screen.dart
+++ flutter/post_2screen.dart
@@ -24,16 +24,13 @@ class ChatScreen2 extends StatefulWidget {
 }
 
 class _ChatScreen2State extends State<ChatScreen2> {
-  int naverLikeCount = 20;
-  int youtubeLikeCount = 20;
-  int textOnlyLikeCount = 10;
-  int additionalLikeCount1 = 15;
-  int additionalLikeCount2 = 5;
+  int youtubeLikeCount = 223;
+  int additionalLikeCount1 = 114;
+  int additionalLikeCount2 = 337;
 
   @override
   Widget build(BuildContext context) {
-    String currentDate =
-        DateFormat('yyyy년 M월 d일').format(DateTime(2024, 10, 29));
+    String currentDate = DateFormat('yyNN년 M월 d일').format(DateTime(20, 10, 29));
 
     return Scaffold(
       appBar: AppBar(
@@ -49,15 +46,15 @@ class _ChatScreen2State extends State<ChatScreen2> {
       backgroundColor: const Color(0xFFC7B1D9),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: ListView(
-          // Column을 ListView로 변경
+        child: Column(
           children: [
             const SizedBox(height: 30),
             Container(
-              width: 80,
+              constraints: const BoxConstraints(maxWidth: 150),
+              alignment: Alignment.center,
               padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
               decoration: BoxDecoration(
-                color: const Color(0xFFEAEAEA),
+                color: const Color(0xFFEEE7F4), // Changed background color
                 borderRadius: BorderRadius.circular(16),
               ),
               child: Text(
@@ -65,115 +62,59 @@ class _ChatScreen2State extends State<ChatScreen2> {
                 style: const TextStyle(
                   fontSize: 14,
                   fontWeight: FontWeight.bold,
-                  color: Colors.black,
+                  color: Colors.black, // Changed text color to black
                 ),
               ),
             ),
             const SizedBox(height: 16),
-            buildChatBubbleWithImage(
-              logoPath: 'assets/img/instagram.png',
-              thumbnailPath: 'assets/img/IU_Ins.png',
-              platform: 'Instagram',
-              videoTitle: "♥제주삼다수 X 아이유\n 11월 달력 大공개!♥",
-              likeCount: youtubeLikeCount,
-              onLikeTap: () {
-                setState(() {
-                  youtubeLikeCount++;
-                });
-              },
-            ),
-            const SizedBox(height: 16),
-            buildChatBubbleWithImage(
-              logoPath: 'assets/img/twitter.png',
-              thumbnailPath: 'assets/img/IU_X(1).png',
-              platform: 'Twitter',
-              videoTitle:
-                  "[IU TV] 장만월 사장의 삼도천 건너기 전 마지막 업무! \n배우 이지은의 스페셜한 \n인터뷰 현장!👩‍❤️‍👩🗣\n\nIU YouTube Channel ▶▶▶ https://youtu.be/kUsCuCiUVs0\n\n#이지은 #아이유 #이지은에게_물어봐",
-              likeCount: additionalLikeCount1,
-              onLikeTap: () {
-                setState(() {
-                  additionalLikeCount1++;
-                });
-              },
-            ),
-            const SizedBox(height: 16),
-            buildChatBubbleWithImage(
-              logoPath: 'assets/img/twitter.png',
-              thumbnailPath: 'assets/img/IU_X(2).png',
-              platform: 'Twitter',
-              videoTitle: "아이유 미니 5집 선공개 곡 \n<Love poem> Teaser Image",
-              likeCount: additionalLikeCount2,
-              onLikeTap: () {
-                setState(() {
-                  additionalLikeCount2++;
-                });
-              },
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildChatBubble({
-    required String logoPath,
-    required String platform,
-    required String content,
-    required int likeCount,
-    required VoidCallback onLikeTap,
-  }) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CircleAvatar(
-          backgroundColor: Colors.white,
-          radius: 16,
-          child: Image.asset(logoPath, width: 24, height: 24), // logoPath 사용
-        ),
-        const SizedBox(width: 8),
-        Expanded(
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+            Expanded(
+              child: ListView(
                 children: [
-                  Text(
-                    platform,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
+                  buildChatBubbleWithImage(
+                    logoPath: 'assets/img/instagram.png',
+                    thumbnailPath: 'assets/img/IU_Ins.png',
+                    platform: 'Instagram',
+                    videoTitle: "♥제주삼다수 X 아이유\n 11월 달력 大공개!♥",
+                    likeCount: youtubeLikeCount,
+                    onLikePressed: () {
+                      setState(() {
+                        youtubeLikeCount++;
+                      });
+                    },
                   ),
-                  const SizedBox(height: 8),
-                  Text(
-                    content,
-                    style: const TextStyle(fontSize: 14, color: Colors.black54),
+                  const SizedBox(height: 16),
+                  buildChatBubbleWithImage(
+                    logoPath: 'assets/img/twitter.png',
+                    thumbnailPath: 'assets/img/IU_X(1).png',
+                    platform: 'Twitter',
+                    videoTitle:
+                        "[IU TV] 장만월 사장의 삼도천 건너기 전 마지막 업무! \n배우 이지은의 스페셜한 \n인터뷰 현장!👩‍❤️‍👩🗣\n\nIU YouTube Channel ▶▶▶ https://youtu.be/kUsCuCiUVs0\n\n#이지은 #아이유 #이지은에게_물어봐",
+                    likeCount: additionalLikeCount1,
+                    onLikePressed: () {
+                      setState(() {
+                        additionalLikeCount1++;
+                      });
+                    },
                   ),
-                  const SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        onTap: onLikeTap,
-                        child: Text(
-                          '$likeCount ♥',
-                          style: const TextStyle(
-                              fontSize: 14, color: Colors.black),
-                        ),
-                      ),
-                    ],
+                  const SizedBox(height: 16),
+                  buildChatBubbleWithImage(
+                    logoPath: 'assets/img/twitter.png',
+                    thumbnailPath: 'assets/img/IU_X(2).png',
+                    platform: 'Twitter',
+                    videoTitle: "아이유 미니 5집 선공개 곡 \n<Love poem> Teaser Image",
+                    likeCount: additionalLikeCount2,
+                    onLikePressed: () {
+                      setState(() {
+                        additionalLikeCount2++;
+                      });
+                    },
                   ),
                 ],
               ),
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 
@@ -183,7 +124,7 @@ class _ChatScreen2State extends State<ChatScreen2> {
     required String platform,
     required String videoTitle,
     required int likeCount,
-    required VoidCallback onLikeTap,
+    required VoidCallback onLikePressed,
   }) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -196,6 +137,7 @@ class _ChatScreen2State extends State<ChatScreen2> {
         const SizedBox(width: 8),
         Expanded(
           child: Card(
+            color: Colors.white, // 카드 배경색을 흰색으로 설정
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
             ),
@@ -225,19 +167,21 @@ class _ChatScreen2State extends State<ChatScreen2> {
                       Text(
                         videoTitle,
                         style: const TextStyle(
-                            fontSize: 14, color: Colors.black54),
+                            fontSize: 14,
+                            color: Colors.black), // Changed color to black
                       ),
                       const SizedBox(height: 8),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          GestureDetector(
-                            onTap: onLikeTap,
-                            child: Text(
-                              '$likeCount ♥',
-                              style: const TextStyle(
-                                  fontSize: 14, color: Colors.black),
-                            ),
+                          Text(
+                            '$likeCount',
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.black),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.favorite, color: Colors.red),
+                            onPressed: onLikePressed,
                           ),
                         ],
                       ),
@@ -245,68 +189,6 @@ class _ChatScreen2State extends State<ChatScreen2> {
                   ),
                 ),
               ],
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget buildTextOnlyChatBubble({
-    required String logoPath,
-    required String platform,
-    required String content,
-    required int likeCount,
-    required VoidCallback onLikeTap,
-  }) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CircleAvatar(
-          backgroundColor: Colors.white,
-          radius: 16,
-          child: Image.asset(logoPath, width: 24, height: 24), // logoPath 사용
-        ),
-        const SizedBox(width: 8),
-        Expanded(
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    platform,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    content,
-                    style: const TextStyle(fontSize: 14, color: Colors.black54),
-                  ),
-                  const SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        onTap: onLikeTap,
-                        child: Text(
-                          '$likeCount ♥',
-                          style: const TextStyle(
-                              fontSize: 14, color: Colors.black),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
             ),
           ),
         ),
